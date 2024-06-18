b8146ebd6eb6b0f8ef990f60f8c0e2c6c96376b9
anirNag <99897287+anirNag@users.noreply.github.com>
2024-06-18 07:53:10 +0200

    log.py

--- log.py
+++ log.py
@@ -1,29 +1,8 @@
-import time
-import board
-import adafruit_dht
+from picamera import PiCamera
+from time import sleep
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D17)
+camera = PiCamera()
 
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
-
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    time.sleep(2.0)
+camera.start_preview()
+sleep(5)
+camera.stop_preview()
